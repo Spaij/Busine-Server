f39da6ced01d6e480c2a8b13881c0f87bbf0c8e3
Gabriel <gabriel1532@hotmail.com>
2018-05-06 22:27:53 -0300

    teste

--- src/main/java/com/busine/model/Organizacao.java
+++ src/main/java/com/busine/model/Organizacao.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 //import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-
+//fdsfsdfs
 //import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
